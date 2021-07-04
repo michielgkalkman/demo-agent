44f8533593549c6318a8a110e1fc36d319171c21
Michiel Kalkman <michiel.kalkman@atos.net>
2021-07-04 20:46:15 +0200

    Playing

--- src/main/java/JavaAgent.java
+++ src/main/java/JavaAgent.java
@@ -1,7 +1,14 @@
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import javassist.CtMethod;
+
 import java.io.File;
-import java.io.FileInputStream;
+import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
+import java.security.ProtectionDomain;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class JavaAgent {
@@ -13,16 +20,114 @@ public class JavaAgent {
      *
      * @param agentArgs       The list of agent arguments
      * @param instrumentation The instrumentation object
-     * @throws InstantiationException While  an instantiation of object cause an error.
      */
-    public static void premain(String agentArgs, Instrumentation instrumentation) throws InstantiationException, UnmodifiableClassException {
+    public static void premain(String agentArgs, Instrumentation instrumentation) throws UnmodifiableClassException {
 
-        log.info(String.format("Starting Java Agent...... %s", agentArgs));
+        log.info(String.format("Starting Java Agent...... %s", agentArgs == null ? "" : agentArgs));
 
+        instrumentation.addTransformer( new ClassFileTransformer() {
+               @Override
+               public byte[] transform(ClassLoader classLoader, String s, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) {
 
-        instrumentation.addTransformer(new TransformerImpl(),true);
+                   log.info(String.format("Transform '%s'", s));
+
+                   if ("java/io/File".equals(s)) {
+                       log.info( String.format("Transforming .... %s", s));
+                       // Javassist
+                       try {
+                           ClassPool cp = ClassPool.getDefault();
+                           CtClass cc = cp.get("java.io.File");
+                           for( CtConstructor m : cc.getConstructors()) {
+                               m.addLocalVariable("elapsedTime", CtClass.longType);
+                               m.insertBefore("elapsedTime = System.currentTimeMillis();");
+                               m.insertAfter("{elapsedTime = System.currentTimeMillis() - elapsedTime;"
+                                       + "System.out.println(\"Method Executed in ms: \" + elapsedTime);}");
+                           }
+                           for( CtMethod m : cc.getDeclaredMethods()) {
+//                           for( CtMethod m : cc.getMethods()) {
+                               log.info(String.format("Processing %s", m.getLongName()));
+                               m.addLocalVariable("elapsedTime", CtClass.longType);
+                               StringBuilder stringBuilder = new StringBuilder();
+                               stringBuilder.append("elapsedTime = System.currentTimeMillis();")
+                                       .append( String.format("System.out.println(\"Method %s(", m.getLongName()));
+
+                               final CtClass[] parameterTypes = m.getParameterTypes();
+                               for(int i = 0; i< parameterTypes.length; i++) {
+                                   if( i>0) {
+                                       stringBuilder.append(',');
+                                   }
+                                   stringBuilder.append(parameterTypes[i].toString());
+                               }
+
+                                stringBuilder.append( ")\");");
+
+                                m.insertBefore( stringBuilder.toString());
+
+//                               m.insertBefore(String.format("elapsedTime = System.currentTimeMillis();"
+//                                               + "System.out.println(\"Method %s Executing\");", m.getLongName()));
+                               m.insertAfter(
+                                       String.format(
+                                       "{elapsedTime = System.currentTimeMillis() - elapsedTime;"
+                                       + "System.out.println(\"Method %s Executed in ms: \" + elapsedTime);}"
+                               , m.getLongName()));
+                           }
+//                           CtMethod m = cc.getDeclaredMethod("exists");
+                           byte[] byteCode = cc.toBytecode();
+                           cc.detach();
+                           return byteCode;
+                       } catch (Exception ex) {
+                           ex.printStackTrace();
+                       }
+                   }
+
+                   return null;
+               }
+           }, true);
+        instrumentation.addTransformer( new ClassFileTransformer() {
+            @Override
+            public byte[] transform(ClassLoader classLoader, String s, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) {
+
+                log.info( String.format("START en STOP .... %s", s));
+                // Javassist
+                try {
+                    ClassPool cp = ClassPool.getDefault();
+                    CtClass cc = cp.get(s);
+                    if( cc == null) {
+                        log.info(String.format(" Cannot find: %s", s));
+                    } else {
+                        log.info(String.format(" .. %s", cc.getName()));
+                        for (CtConstructor m : cc.getConstructors()) {
+                            m.insertBefore(String.format(
+                                    "System.out.println(\"START %s\");", m.getLongName()));
+                            m.insertAfter(String.format(
+                                    "System.out.println(\"END   %s\");", m.getLongName()));
+                        }
+                        for (CtMethod m : cc.getDeclaredMethods()) {
+                            m.insertBefore(String.format(
+                                    "System.out.println(\"START %s\");", m.getLongName()));
+                            m.insertAfter(String.format(
+                                    "System.out.println(\"END   %s\");", m.getLongName()));
+                        }
+                    }
+//                           CtMethod m = cc.getDeclaredMethod("exists");
+                    byte[] byteCode = cc.toBytecode();
+                    cc.detach();
+                    return byteCode;
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+                return null;
+            }
+        }, true);
+
+        log.info(String.format("Is retransformation supported: %b", instrumentation.isRetransformClassesSupported()));
+
+        log.info(String.format("Is File modifiable: %b", instrumentation.isModifiableClass(File.class)));
 
         instrumentation.retransformClasses(
                 File.class);
+
+
     }
 }
